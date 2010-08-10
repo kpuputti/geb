c94635b8e0e59620b449fd4fad6bdef4aceb7000
Kimmo Puputti <kpuputti@gmail.com>
2010-08-10 21:12:30 +0300

    Added basic structure to the files.

---
+++ crab.py
@@ -0,0 +1,12 @@
+"""\
+crab.py: CRAB Recursive Acronym Broadener
+"""
+import sys
+
+
+def main(args):
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
---
+++ quine.py
@@ -0,0 +1,12 @@
+"""\
+quine.py: A Quine (programs that outputs its own source code).
+"""
+import sys
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
